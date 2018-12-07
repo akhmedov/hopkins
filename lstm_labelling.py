0dcfec327476ec40f2c2662573a18c36e8856ccd
Rolan Akhmedov <rolan.kharkiv@gmail.com>
2018-12-07 19:36:57 +0200

    series labeling with recurent model

--- lstm_labelling.py
+++ lstm_labelling.py
@@ -1,59 +1,69 @@
 #
-#  mlp_binary_classification.py
+#  lstm_binary_labelling.py
 #  Hopkins
 #
-#  Created by Rolan Akhmedov on 22.09.18.
+#  Created by Rolan Akhmedov on 05.07.18.
 #  Copyright © 2018 Rolan Akhmedov. All rights reserved.
 #
 
-from __future__ import print_function
-
-import sys, keras
 import numpy as np
-from keras.models import Sequential
-from keras.layers import Dense, Dropout
-from keras.optimizers import RMSprop
+import gc
 
 from dataset import Dataset, plot
 from dataset import check_balance, make_balance
 
+from keras.preprocessing import sequence
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Embedding
+from keras.layers import LSTM, SpatialDropout1D
+from keras.layers import TimeDistributed
+
 # load train and test data
-ds = Dataset('dataset.json')
+ds = Dataset('test.json')
 ds.norm_amplitude()
 window_size = ds.max_pulse_wides()
 X, Y = ds.split(window_length=window_size, step=1)
 
-# prepare data for learning in keras
-for y in Y: y[0] = 0 if y[1] else 1
+# prepare data for learning in keras one-to-one lstm model
+for y in Y: y[0] = 0 if sum(y) > 1 else 1
 x_train = np.array(X[:int(9*len(X)/10)]).astype('float32')
 y_train = np.array(Y[:int(9*len(Y)/10)]).astype('float32')
 x_test  = np.array(X[int(9*len(X)/10):]).astype('float32')
 y_test  = np.array(Y[int(9*len(Y)/10):]).astype('float32')
 # x_train, y_train = make_balance(x_train, y_train)
-if len(x_train) != len(y_train) or len(x_test) != len(y_test):
-	sys.exit('Error: electromagnetic dataset size does not match!')
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
 
-# print dataset info
 print('_________________________________________________________________')
 print('Pulse wides (sempales): {} - min, {} - max'.format(ds.min_pulse_wides(), ds.max_pulse_wides()))
 print('Size: {} - number of sempales, {} - number of sparks'.format(ds.length(), ds.sparks()))
 print('Train: {} - size, {} - balance'.format(len(y_train), check_balance(y_train)))
 print('Test: {} - size, {} - balance'.format(len(y_test), check_balance(y_test)))
 print('_________________________________________________________________')
+gc.collect()
 
-# architecture of fully connected neural network
+batch_size = 1
 model = Sequential()
-model.add(Dense(int(window_size/2), activation='relu', input_shape=(window_size,)))
-model.add(Dense(2, activation='softmax'))
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.add(LSTM(window_size, batch_input_shape=(batch_size, window_size, 1), stateful=True))
+model.add(Dropout(0.5))
+model.add(Dense(3,activation='softmax'))
+model.compile(loss='mean_squared_error', optimizer='rmsprop', metrics=['accuracy'])
 
-# learning proccess
 history = model.fit(x_train, y_train,
-					batch_size=256, epochs=20, verbose=1,
+					batch_size=batch_size, epochs=1, verbose=1,
 					validation_data=(x_test, y_test))
 
-# test the model on test data
 model.summary()
-score = model.evaluate(x_test, y_test, verbose=0)
+score = model.evaluate(x_test, y_test, batch_size=batch_size, verbose=1)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
+
+# x_input = np.zeros((1, 100))
+# x_input[0, :] =  x[t+i: t+i+101]
+# for i in range(10)
+# 	y_output = model.predict(x_input)
+# 	print(y_output)
+# 	x_input[0, 0:100] =  x[t+i+1: t+i+100]
+# 	x_input[100] = y_output
+
+
